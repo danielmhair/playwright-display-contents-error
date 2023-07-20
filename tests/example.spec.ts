e6248dfc198b4cd4ba14dbaf68c4b3e84ed5c79d
Daniel Hair <danielmhair@gmail.com>
2023-07-20 13:20:32 +0000

    fix: update spec

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,27 @@
 import { test, expect } from '@playwright/test'
 import { resolve } from 'path'
 
+test('non-visible element should throw Element Not Found or something similiar, but doesnt', async ({ page }) => {
+  const absoluteFilePath = resolve('tests/index.html')
+  await page.goto(`file://${absoluteFilePath}`)
+  // Expected this to throw Element Not Found, because element should not be visible
+  expect(await page.$('.tempPositionWrapper')).toThrow()
+  // await tempPositionWrapper.screenshot({ path: 'tempPositionWrapper.png' })
+})
+
 test('Take screenshot of non-visible element and expect Element Not Found, but instead a screenshot error occurred', async ({ page }) => {
-  const absoluteFilePath = resolve('index.html')
+  const absoluteFilePath = resolve('tests/index.html')
+  await page.goto(`file://${absoluteFilePath}`)
+  // Expected this to throw Element Not Found, because element should not be visible
+  const tempPositionWrapper = await page.$('.tempPositionWrapper')
+  await tempPositionWrapper?.screenshot({ path: 'tempPositionWrapper.png' })
+})
+
+test('For elements with display contents set, element has no width or height, meaning it is not visible, so isVisible() should be false', async ({ page }) => {
+  const absoluteFilePath = resolve('tests/index.html')
   await page.goto(`file://${absoluteFilePath}`)
   // select the element with class tempPositionWrapper
   const tempPositionWrapper = await page.$('.tempPositionWrapper')
-  // take a screenshot of the element
-  await tempPositionWrapper?.screenshot({ path: 'tempPositionWrapper.png' })
+  const visible = await tempPositionWrapper?.isVisible()
+  expect(visible).toBe(false)
 })
